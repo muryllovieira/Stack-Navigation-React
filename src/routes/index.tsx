811def21834bc57364696b075b06ef1155917f4a
muryllovieira <muryllovieira59@gmail.com>
2025-03-08 11:55:20 -0300

    trabalhando com Drawer Navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,26 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StackRoutes } from "./stack.routes";
-import { BottomTabsRoutes } from './bottom-tabs.routes' 
+import { BottomTabsRoutes } from './bottom-tabs.routes'
+import { DrawerRoutes } from "./drawer.routes";
 
 //CAIXA DE CONTEXTO
 export function Routes() {
     return (
-        <NavigationContainer>
-            <BottomTabsRoutes />
-        </NavigationContainer>
+
+        /* NAVEGAÇÃO COM STACK */
+        // <NavigationContainer>
+        //    <StackRoutes />
+        // </NavigationContainer>
+
+        /* NAVEGAÇÃO COM TABS */
+        //<NavigationContainer>
+        //<BottomTabsRoutes />
+        //</NavigationContainer> 
+
+        /* NAVEGAÇÃO COM DRAWER */
+        < NavigationContainer >
+            <DrawerRoutes />
+        </NavigationContainer >
     )
 
 }
